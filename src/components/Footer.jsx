a36c86d2586b48fc0b72e19b2f7edd904b3f5e28
Thomas <thomasrlovin@gmail.com>
2026-09-23 18:44:43 -0400

    Fixed JSX expression in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,9 @@ import React from "react";
 function Footer(date) {
   return (
     <footer>
-      <p> Copyright © {date}</p>
+      <p> Copyright © {new Date().getFullYear()}</p>
     </footer>
   );
 }
-
-const date = new Date();
-const currentYear = date.getFullYear();
-
-Footer(currentYear);
 
 export default Footer;
