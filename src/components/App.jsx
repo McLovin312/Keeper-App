f2a439c7647d7e6b9ef3ac67d2e42d4b7331a88a
Thomas <thomasrlovin@gmail.com>
2026-09-25 16:58:39 -0400

    Notes in array now render, added props to notes, allow vite to access on network

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import { notes } from "../notes";
+
+const newNotes = notes.map((newNote) => (
+  <Note key={newNote.key} title={newNote.title} content={newNote.content} />
+));
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {newNotes}
       <Footer />
     </div>
   );
